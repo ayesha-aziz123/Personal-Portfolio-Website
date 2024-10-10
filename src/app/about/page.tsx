5d9b992e423bd39542a8d36a2fbe6817bc772ad0
Ayesha Aziz <163914368+ayesha-aziz123@users.noreply.github.com>
2024-10-10 20:46:33 +0500

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from '../components/about'
+import Footer from '../components/footer'
+const page = () => {
+    return (
+      <div>
+     <About/>
+     <Footer/>
+     </div>
+    )
+  }
+
+export default page
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../components/footer'
+import Projects from '../components/projects'
+const page = () => {
+    return (
+      <div>
+     <Projects/>
+     <Footer/>
+     </div>
+    )
+  }
+
+export default page
